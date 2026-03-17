037fe9cc9d3ffe66605928e1a1681af05cb0287e
Johanna Nguyen <johannanguyen@gmail.com>
2026-03-17 16:05:39 +0100

    Configure OpenAPI bearerAuth security scheme

--- src/main/java/com/project/redflag/config/OpenAPIConfig.java
+++ src/main/java/com/project/redflag/config/OpenAPIConfig.java
@@ -1,19 +1,29 @@
 package com.project.redflag.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
     @Bean
-    public OpenAPI customOpenAPI(){
-        return new OpenAPI().info(
-                new Info()
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
                         .title("Red Flag Report API")
                         .version("1.0")
-                        .description("Srping Boot RESTful API for Red Flag Report Application")
-        );
+                        .description("Spring Boot RESTful API for Red Flag Report Application"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
